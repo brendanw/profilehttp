e36cb999d87f41f3eba9c0f7cd015de9d9c322fe
Brendan <brendandubs@gmail.com>
2019-12-18 07:22:22 -0800

    Give initial delay in shell script to allow server to get built and running

--- runtest.sh
+++ runtest.sh
@@ -2,5 +2,5 @@
 
 cd server
 # Start server
-./gradlew run & cd .. && cd client && ./gradlew -PmainClass=KtorKt run ; ./gradlew -PmainClass=OkhttpKt run ; cd .. && cd server && ./gradlew --stop
+./gradlew run & sleep 5 && cd .. && cd client && ./gradlew -PmainClass=KtorKt run ; ./gradlew -PmainClass=OkhttpKt run ; cd .. && cd server && ./gradlew --stop
 lsof -ti:5003 | xargs kill
